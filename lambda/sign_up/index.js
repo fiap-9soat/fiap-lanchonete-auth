4a7b34f134934eb92a03640b05089d8657c1a525
Lamarcke <cassiolamarcksilvafreitas@gmail.com>
2025-03-13 01:24:59 -0300

    - altera parametrização do lambda de signUp

--- lambda/sign_up/index.js
+++ lambda/sign_up/index.js
@@ -6,12 +6,39 @@ This is necessary since we don't have a dedicated frontend client.
 const AWS = require('aws-sdk');
 const cognito = new AWS.CognitoIdentityServiceProvider();
 
+function validaCPF(cpf) {
+    let Soma;
+    let Resto;
+    Soma = 0;
+    if (cpf === "00000000000") return false;
+
+    if (cpf.length !== 11){
+        return false;
+    }
+
+    for (let i=1; i<=9; i++) Soma = Soma + parseInt(cpf.substring(i-1, i)) * (11 - i);
+    Resto = (Soma * 10) % 11;
+
+    if ((Resto == 10) || (Resto == 11))  Resto = 0;
+    if (Resto != parseInt(cpf.substring(9, 10)) ) return false;
+
+    Soma = 0;
+    for (i = 1; i <= 10; i++) Soma = Soma + parseInt(cpf.substring(i-1, i)) * (12 - i);
+    Resto = (Soma * 10) % 11;
+
+    if ((Resto == 10) || (Resto == 11))  Resto = 0;
+    if (Resto != parseInt(cpf.substring(10, 11) ) ) return false;
+    return true;
+}
+
 exports.handler = async (event) => {
     const {email, cpf, password} = JSON.parse(event.body);
 
-    // TODO: validar CPF!
+    if (!validaCPF(cpf)){
+        throw new Error("CPF inválido. CPF deve ser somente números!")
+    }
 
-    const params = {
+    const userCreateParams = {
         UserPoolId: process.env.USER_POOL_ID,
         Username: email,
         UserAttributes: [
@@ -22,8 +49,19 @@ exports.handler = async (event) => {
         TemporaryPassword: password,
     };
 
+    const setPasswordParams = {
+        Password: password,
+        UserPoolId: process.env.USER_POOL_ID,
+        Username: email,
+        Permanent: true, // Set password as permanent
+    };
+
     try {
-        const signUpResponse = await cognito.adminCreateUser(params).promise();
+        const signUpResponse = await cognito.adminCreateUser(userCreateParams).promise();
+
+        // Set the password as permanent to avoid password reset
+        await cognito.adminSetUserPassword(setPasswordParams).promise();
+
         return {
             statusCode: 200,
             body: JSON.stringify(signUpResponse),
--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+locals {
+  iam_role_arn       = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/LabRole"
+}
+
+data "aws_caller_identity" "current" {}
+
 module "cognito" {
   source = "./modules/cognito"
   # Must be unique
@@ -9,7 +15,7 @@ module "sign_up_lambda" {
   function_name = "signUpFunction"
   filename      = "lambda/sign_up/lambda_function_payload.zip"
   handler       = "index.handler"
-  role          = var.role_arn
+  role          = local.iam_role_arn
   environment = {
     USER_POOL_ID = module.cognito.user_pool_id
   }
@@ -20,7 +26,7 @@ module "sign_in_lambda" {
   function_name = "signInFunction"
   filename      = "lambda/sign_in/lambda_function_payload.zip"
   handler       = "index.handler"
-  role          = var.role_arn
+  role          = local.iam_role_arn
   environment = {
     USER_POOL_ID = module.cognito.user_pool_id
     CLIENT_ID    = module.cognito.client_id
@@ -32,7 +38,7 @@ module "password_recovery_lambda" {
   function_name = "passwordRecoveryFunction"
   filename      = "lambda/password_recovery/lambda_function_payload.zip"
   handler       = "index.handler"
-  role          = var.role_arn
+  role          = local.iam_role_arn
   environment = {
     CLIENT_ID = module.cognito.client_id
   }
